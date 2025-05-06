9df86b125b5da48e243ab0eec323ea1c7d52b5bd
Ahmed Agha <agha.ah@northeastern.edu>
2025-05-05 21:01:06 -0400

    Revert "don't scale discrete latents"
    This reverts commit 22560f05cd1316cc118d7a23149decb75da06007.

--- dreamerv3/nets.py
+++ dreamerv3/nets.py
@@ -53,9 +53,9 @@ class RSSM(nj.Module):
       self.init_equiv_nets(key)
 
   def init_equiv_nets(self, key):    
-    stoch = self._stoch
-    deter = self._deter
     units = self._kw['units'] // self._grp.scaler
+    stoch = self._stoch // self._grp.scaler
+    deter = self._deter // self._grp.scaler
     gspace = self._grp.grp_act
     if self._classes:
       self._field_type_stoch  = nn.FieldType(gspace, stoch * self._classes * [gspace.regular_repr])
@@ -128,23 +128,25 @@ class RSSM(nj.Module):
     self.init_gru_cell = nn.R2Conv(**gru_kw)
 
   def initial(self, bs):
-    if self._equiv:
-      stoch = self._stoch * self._grp.scaler
-      deter = self._deter * self._grp.scaler
-    else:
-      stoch = self._stoch
-      deter = self._deter
     if self._classes:
       state = dict(
-          deter=jnp.zeros([bs, deter], f32),
-          logit=jnp.zeros([bs, stoch, self._classes], f32),
-          stoch=jnp.zeros([bs, stoch, self._classes], f32))
+          deter=jnp.zeros([bs, self._deter], f32),
+          logit=jnp.zeros([bs, self._stoch, self._classes], f32),
+          stoch=jnp.zeros([bs, self._stoch, self._classes], f32))
     else:
-      state = dict(
-        mean=jnp.zeros([bs, stoch], f32),
-        std=jnp.ones([bs, stoch], f32),
-        stoch=jnp.zeros([bs, stoch], f32), 
-        deter=jnp.zeros([bs, deter], f32))
+      if self._equiv:
+        #TODO:extend to other types of equivariance
+        state = dict(
+          mean=jnp.zeros([bs, self._stoch], f32),
+          std=jnp.ones([bs, self._stoch], f32),
+          stoch=jnp.zeros([bs, self._stoch], f32),
+          deter=jnp.zeros([bs, self._deter], f32))
+      else:
+        state = dict(
+          mean=jnp.zeros([bs, self._stoch], f32),
+          std=jnp.ones([bs, self._stoch], f32),
+          stoch=jnp.zeros([bs, self._stoch], f32), 
+          deter=jnp.zeros([bs, self._deter], f32))
     if self._initial == 'zeros':
       return cast(state)
     elif self._initial == 'learned':
@@ -221,7 +223,7 @@ class RSSM(nj.Module):
       prev_action *= sg(self._action_clip / jnp.maximum(
           self._action_clip, jnp.abs(prev_action)))
     if self._classes:
-      shape = prev_stoch.shape[:-2] + (self._stoch * self._grp.scaler * self._classes,)
+      shape = prev_stoch.shape[:-2] + (self._stoch * self._classes,)
       prev_stoch = prev_stoch.reshape(shape)
     if len(prev_action.shape) > len(prev_stoch.shape):  # 2D actions.
       shape = prev_action.shape[:-2] + (np.prod(prev_action.shape[-2:]),)
@@ -323,7 +325,7 @@ class RSSM(nj.Module):
 
   def _stats(self, name, x):
     if self._classes:
-      if self._equiv:        
+      if self._equiv:
         flat_logits = self.get(f'{name}', 
                       EquivLinear, 
                       **{"net":self.init_stoch_mean, 
@@ -331,7 +333,7 @@ class RSSM(nj.Module):
                         'out_type':self._field_type_stoch,
                         'norm':'none',
                         'act': 'none'})(x)
-        logit = jnp.stack(jnp.split(flat_logits, self._stoch * self._grp.scaler, -1), 1)
+        logit = jnp.stack(jnp.split(flat_logits, self._stoch , -1), 1)
         logit = logit.reshape(x.shape[:-1] + logit.shape[-2:])
       else:
         x = self.get(name, Linear, self._stoch * self._classes)(x)
@@ -353,7 +355,7 @@ class RSSM(nj.Module):
                         'act':'none'})(x).tensor.mean(-1).mean(-1)
         x = nn.GeometricTensor(x[:, :, jnp.newaxis, jnp.newaxis], self._field_type_embed)
         x = self._embed_group_pooling(x).tensor.mean(-1).mean(-1)
-        std = self.get('stoch_std', Linear, self._stoch)(x)
+        std = self.get('stoch_std', Linear, self._stoch // self._grp.scaler)(x)
         std = jnp.repeat(std, self._grp.scaler, -1)
       else:
         x = self.get(name, Linear, 2 * self._stoch)(x)
@@ -685,6 +687,7 @@ class EquivImageEncoder(nj.Module):
 
   def __init__(self, depth, grp, key, **kw):
     gspace = grp.grp_act
+    depth = depth // grp.scaler
     self.feat_type_in  = nn.FieldType(gspace,  3*[gspace.trivial_repr])
     self.feat_type_out1  = nn.FieldType(gspace,  depth*[gspace.regular_repr])
     depth *= 2
@@ -962,7 +965,7 @@ class InvMLP(MLP):
                        symlog_inputs=symlog_inputs, **kw)
     
       r2_act = grp.grp_act
-      self.feat_type_in = nn.FieldType(r2_act, (deter + stoch) * [r2_act.regular_repr])
+      self.feat_type_in = nn.FieldType(r2_act, (deter // grp.scaler + stoch // grp.scaler) * [r2_act.regular_repr])
       self.group_pooling = pooling_module(self.feat_type_in, name='group_pooling')      
 
   def __call__(self, inputs):
@@ -984,7 +987,7 @@ class EquivMLP(MLP):
                        inputs=inputs, dims=dims, 
                        symlog_inputs=symlog_inputs, **kw)
       r2_act = grp.grp_act
-      self.feat_type_in = nn.FieldType(r2_act, (deter + stoch) * [r2_act.regular_repr])
+      self.feat_type_in = nn.FieldType(r2_act, (deter // grp.scaler + stoch // grp.scaler) * [r2_act.regular_repr])
       self.feat_type_hidden  = nn.FieldType(r2_act,  units*[r2_act.regular_repr])
       keys = jax.random.split(key, 7)
       self.escnn1 = econv_module(in_type=self.feat_type_in, 
