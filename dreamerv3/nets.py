cf288ce29b194d64486f827303167e6162167103
Ahmed Agha <agha.ah@northeastern.edu>
2025-07-20 12:00:05 -0400

    adjust scaling

--- dreamerv3/nets.py
+++ dreamerv3/nets.py
@@ -1306,7 +1306,7 @@ class InvMLP(MLP):
         r2_act = grp.grp_act
         self.feat_type_in = nn.FieldType(
             r2_act,
-            int(deter // (grp.scaler**0.5) + stoch // (grp.scaler**0.5))
+            int(deter // (grp.scaler**0.25) + stoch // (grp.scaler**0.25))
             * [r2_act.regular_repr],
         )
         self.feat_type_hidden = nn.FieldType(r2_act, units * [r2_act.regular_repr])
