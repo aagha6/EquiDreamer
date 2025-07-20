fix scaling

dreamerv3/nets.py
@@ -1306,10 +1306,12 @@ class InvMLP(MLP):
         r2_act = grp.grp_act
         self.feat_type_in = nn.FieldType(
             r2_act,
-            int(deter // (grp.scaler**0.25) + stoch // (grp.scaler**0.25))
+            int(deter // (grp.scaler**0.5) + stoch // (grp.scaler**0.5))
             * [r2_act.regular_repr],
         )
-        self.feat_type_hidden = nn.FieldType(r2_act, units * [r2_act.regular_repr])
+        self.feat_type_hidden = nn.FieldType(
+            r2_act, int(units // (grp.scaler**0.25)) * [r2_act.regular_repr]
+        )
         self.escnn1 = econv_module(
             in_type=self.feat_type_in,
             out_type=self.feat_type_hidden,